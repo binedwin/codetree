0bccf4523d365fa5186112739ee4f51de31d0a5e
binedwin <127584490+binedwin@users.noreply.github.com>
2025-05-18 22:27:20 +0900

    Title: 특정 문자의 유무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250518/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,14 @@
+# 문자열을 입력받습니다.
+string = input()
+
+# 문자열 'ee'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
+if 'ee' in string:
+	print("Yes", end=" ")
+else:
+	print("No", end=" ")
+
+# 문자열 'ab'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
+if 'ab' in string:
+	print("Yes")
+else:
+	print("No")
