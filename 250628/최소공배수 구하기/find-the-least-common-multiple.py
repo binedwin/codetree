57e47ef23d57a9e714a9d6216332111e73243427
binedwin <127584490+binedwin@users.noreply.github.com>
2025-06-28 19:02:16 +0900

    Title: 최소공배수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250628/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250628/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,9 +1,15 @@
-n, m = map(int, input().split())
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 
-def a():
-    for i in range(max(n,m),n*m):
-        if i%n==0 and i%m==0:
-            print(i)
-            break
 
-a()
+# n과 m의 최소공배수를 출력합니다.
+def find_lcm(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+
+    print(n * m // gcd)
+   
+
+find_lcm(n, m)
