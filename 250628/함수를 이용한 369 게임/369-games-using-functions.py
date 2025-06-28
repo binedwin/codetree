9676481d43f4abf7a297eab3e3e6f90cb8c97f01
binedwin <127584490+binedwin@users.noreply.github.com>
2025-06-28 20:28:12 +0900

    Title: 함수를 이용한 369 게임, Time: 83ms, Memory: 18MB, Status: Passed - Codetree

--- 250628/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250628/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,14 +2,16 @@ a, b = map(int, input().split())
 cnt=0
 
 def syg(n):
-    return n % 10 == 3 or n % 10 == 6 or n % 10 == 9 or n//10==3 or n//10==6 or n//10==9
+    cnt=0
+    for n in str(n):
+        if n in '369':
+            return True
 
 def sam(n):
     return n%3==0 or syg(n)
-
-
-for i in range(a,b+1):
-    if sam(i):
+        
+for i in range(a, b+1):
+    if i%3==0 or syg(i):
         cnt+=1
 
-print(cnt)
+print( cnt)
