3cc813c1108f8bb58c0c4e28e866ae2a012631b4
binedwin <127584490+binedwin@users.noreply.github.com>
2025-07-02 14:24:13 +0900

    Title: palindrome 여부 판단하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250702/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 250702/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -2,10 +2,10 @@ A = input()
 
 def palindrome(s):
     for i in range(len(s)):
-        if s[i] == s[len(s)- i -1]:
-            return True
+        if s[i] != s[len(s)- i -1]:
+            return False
 
-    return False
+    return True
 
 if palindrome(A):
     print("Yes")
