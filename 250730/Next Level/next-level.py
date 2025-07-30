2b06896e601eeda693f6c4b53f7f1bc65edf41e2
binedwin <127584490+binedwin@users.noreply.github.com>
2025-07-30 13:45:16 +0900

    Title: Next Level, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250730/Next Level/next-level.py
@@ -0,0 +1,11 @@
+user2_id, user2_level = input().split()
+user2_level = int(user2_level)
+
+class k:
+    def __init__(self,a="",b=0):
+        self.a=a
+        self.b=b
+        print(f"user {a} lv {b}")
+
+user1 =k("codetree",10)
+userlevel=k(user2_id, user2_level)
