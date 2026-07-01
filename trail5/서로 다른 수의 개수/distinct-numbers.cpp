b381e70030c99716c3c50c1ef6b8f161a32ce17f
binedwin <127584490+binedwin@users.noreply.github.com>
2026-07-01 16:07:55 +0900

    [Passed] 서로 다른 수의 개수 (32ms, 2MB)

--- trail5/서로 다른 수의 개수/distinct-numbers.cpp
+++ trail5/서로 다른 수의 개수/distinct-numbers.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <unordered_set>
+
+#define MAX_N 100000
+
 using namespace std;
 
+// 변수 선언
+int n;
+int arr[MAX_N];
+unordered_set<int> s;
+
 int main() {
-    int n;
-    cin>> n;
-    unordered_set<int> u_s;
-    for(int i=0; i<n; i++){
-        int a;
-        cin >>a;
-        u_s.insert(a);
-    }
-    int cnt=0;
-    for(auto a : u_s){
-        cnt++;
-    }
-    cout<<cnt;
+    // 입력:
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // HashSet에 원소를 추가합니다.
+    for(int i = 0; i < n; i++)
+        s.insert(arr[i]);
+    
+    // HashSet의 크기를 출력합니다.
+    cout << s.size();
     return 0;
 }
